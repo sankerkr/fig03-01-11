6ecbe6b87036811941111b165eac5c06126ed042
Paul Deitel <pauldeitel@PJDRetina15.local>
2014-06-30 11:43:41 -0400

    Initial Commit

---
+++ fig03-01-11/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  fig03-01-11
+//
+//  Created by Paul Deitel on 6/30/14.
+//  Copyright (c) 2014 Paul Deitel. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
